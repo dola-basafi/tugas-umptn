2e3baee53bfce261fa67610a46dc968bad8b5994
Abdullah  Mochammad Basafi <dola.doit@gmail.com>
2022-11-14 21:59:43 +0700

    tugas-slicing-css

---
+++ tailwind/script.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    fontFamily:{
+      'sans':['Inter']
+    },
+    container: {
+      center:true,
+      screens: {        
+        xl: "1170px",
+      },
+    },
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+};
